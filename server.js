4f4fc246475cfc52b48a92e5e14df28afc4f630a
Lucas-bit <lucasbertaudgonzalez@gmail.com>
2020-03-13 16:00:39 -0400

    moved public and seeders foldes outside of containing folder to root directory

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-// const User = require("./userModel.js");
+// const User = require("index.js");
 const app = express();
 
 app.use(logger("dev"));
@@ -14,7 +14,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+
+app.get("/", (_req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
 
 app.post("/submit", ({body}, res) => {
   User.create(body)
